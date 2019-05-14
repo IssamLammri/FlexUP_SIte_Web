9e6f6956f938fc2f8f4e07b75561d0f0d249a3cf
li184279 <issam.lammri@etu.univ-lehavre.fr>
2019-05-14 16:13:58 +0200

    first Project

---
+++ src/Controller/MyGroups.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MyGroups extends  AbstractController {
+
+    /**
+     * @Route("/mygroups",name="mygroups.index")
+     * @return Response
+     */
+    public function index():Response{
+        return $this->render('Groups/mygroups.html.twig');    }
+}
